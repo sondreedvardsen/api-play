ea4040b0eafff6f248fa0e8de51ff0c248434545
Sondre Edvardsen <sondre@mystore.no>
2017-01-10 07:13:51 +0100

    use token

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,14 +12,20 @@ export class LoginComponent implements OnInit {
 	private token: string;
   private sub: any;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+		private router: Router,
+		private route: ActivatedRoute
+	) {
 		this.authUrl = 'https://auth.mystore.no/oauth/authorize?client_id=9&redirect_uri=https://s3-eu-west-1.amazonaws.com/api-auth-demo/callback.html&response_type=code&scope=read:products read:categories';
 	}
 
   ngOnInit() {
 		this.sub = this.route.params.subscribe(params => {
-       this.token = params['token'];
-       localStorage.setItem('apiToken', this.token);
+			this.token = params['token'];
+			if(this.token != null) {
+ 				localStorage.setItem('apiToken', this.token);
+				this.router.navigate(['/']);
+			}
     });
   }
 
