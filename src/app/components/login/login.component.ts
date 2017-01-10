95ae87e7b4bce09678fafdc6d41c14a18a3c37d5
Sondre Edvardsen <sondre@mystore.no>
2017-01-10 05:45:44 +0100

    asd

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,7 +10,7 @@ export class LoginComponent implements OnInit {
 	authUrl: string;
 
   constructor() {
-		this.authUrl = 'https://auth.mystore.no/oauth/authorize?client_id=9&redirect_uri=https://sondreedvardsen.github.io/api-play/login/&response_type=code&scope=*';
+		this.authUrl = 'https://auth.mystore.no/oauth/authorize?client_id=9&redirect_uri=https://sondreedvardsen.github.io/api-play/login/&response_type=code&scope=read:products read:categories';
 	}
 
   ngOnInit() {
