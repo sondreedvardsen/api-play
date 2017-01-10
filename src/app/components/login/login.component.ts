703902f15517a4475534fd2b829a8a9e6af45a97
Sondre Edvardsen <sondre@mystore.no>
2017-01-10 06:56:12 +0100

    scope

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
   private sub: any;
 
   constructor(private route: ActivatedRoute) {
-		this.authUrl = 'https://auth.mystore.no/oauth/authorize?client_id=9&redirect_uri=https://s3-eu-west-1.amazonaws.com/api-auth-demo/callback.html&response_type=code&scope=*';
+		this.authUrl = 'https://auth.mystore.no/oauth/authorize?client_id=9&redirect_uri=https://s3-eu-west-1.amazonaws.com/api-auth-demo/callback.html&response_type=code&scope=read:products read:categories';
 	}
 
   ngOnInit() {
