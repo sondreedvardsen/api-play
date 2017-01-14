33201f0f186d31aff0ed546bfdc007255ff38c24
Sondre Edvardsen <sondre@mystore.no>
2017-01-14 21:00:08 +0100

    'categories'

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CatalogService } from '../../services/catalog.service';
+import { MdSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-catalog',
@@ -9,17 +10,44 @@ import { CatalogService } from '../../services/catalog.service';
 export class CatalogComponent implements OnInit {
 
 	categories;
+	cat;
 	errorMessage: string;
 
-  constructor(private catalogService: CatalogService) { }
+  constructor(
+		private catalogService: CatalogService,
+		public snackBar: MdSnackBar
+	) { }
 
   ngOnInit() {
 		this.getCategories();
   }
+
 	getCategories() {
-  	this.catalogService.categories()
-									     .subscribe(
-													categories => this.categories = categories,
-													error =>  this.errorMessage = <any>error);
+  	this.catalogService.getCategories()
+			.subscribe(
+				categories => {
+					this.categories = categories;
+					this.buildCategories();
+				},
+				error => {
+					this.errorMessage = <any>error;
+					this.snackBar.open(this.errorMessage, '',
+						{
+							duration: 5000
+						}
+					)
+				}
+			);
+	}
+
+	buildCategories() {
+		for(let category of this.categories) {
+			let parentId = category.attributes.parent_id;
+			if(parentId != 0) {
+				let parent = this.categories.findIndex(x => x.id == parentId);
+				this.categories[parent].attributes.children.push(category.id);
+			}
+		}
+		this.cat = this.categories;
 	}
 }
--- src/app/services/catalog.service.ts
+++ src/app/services/catalog.service.ts
@@ -11,15 +11,37 @@ export class CatalogService {
 		private authService: AuthService
 	) {	}
 
-	categories(): Observable<any[]> {
-		let headers = new Headers({
-																'Content-Type': 'application/vnd.api+json',
-																'Accept': 'application/vnd.api+json',
-																'Authorization': 'Bearer ' + this.authService.accessToken
-															});
-    let options = new RequestOptions({ headers: headers });
-    return this.http
-               .get('https://api.mystore.no/shops/' + this.authService.host + '/categories', options)
-               .map((res: Response) => res.json().data);
+	getCategories(): Observable<any[]> {
+		let url = 'https://api.mystore.no/shops/' + this.authService.host;
+		let headers = new Headers(
+			{
+				'Content-Type': 'application/vnd.api+json',
+				'Accept': 'application/vnd.api+json',
+				'Authorization': 'Bearer ' + this.authService.accessToken
+			}
+		);
+		let options = new RequestOptions({ headers: headers });
+		return this.http
+			.get(url + '/categories', options)
+			.map(this.extractData)
+      .catch(this.handleError);
+  }
+
+	private extractData(res: Response) {
+    let body = res.json();
+    return body.data || { };
+  }
+
+  private handleError(error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      let body = error.json() || '';
+      let err = body.errors[0] || JSON.stringify(body);
+      errMsg = `${err.status} - ${err.title}: ${err.detail}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
   }
 }
